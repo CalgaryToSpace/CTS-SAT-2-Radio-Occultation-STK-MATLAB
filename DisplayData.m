752be75ae662aaf0b4340331a5b2f162067163ea
Li Tan <robloxcity88@gmail.com>
2026-06-23 19:18:00 -0600

    Changed output file structure and split code into DisplayData.m and GetFileFormat.m

---
+++ DisplayData.m
@@ -0,0 +1,68 @@
+clear
+close all
+
+% List of data to run simulations (assumes first and second columns are always start/stop times)
+ELEMENTS = {'StartTime'; 'StopTime'; 'ToStartLat'; 'ToStopLat'};
+STARTDATE = '5-May-2026 18:00:00';
+
+% Read in table
+opt = GetFileFormat("Output\Combined\occultation_2weeks", ELEMENTS);
+combinedOccultation = readtable("Output\Combined\occultation_2weeks", opt);
+
+% Get durations
+occultationDurations = combinedOccultation{:,2} - combinedOccultation{:,1};
+
+% Filter for durations above 5s
+condition = occultationDurations > seconds(5);
+combinedOccultation = combinedOccultation(condition(:,1),:);
+occultationDurations = occultationDurations(condition(:,1),:);
+
+% Calculate some statistics ----------------------------------------------%
+totalOccultationDuration =  sum(occultationDurations);
+percentOccultation = hours(totalOccultationDuration) / (24*14);
+dailyOccultationAvg = totalOccultationDuration / 14; % hours
+occultationAvg = mean(occultationDurations);
+
+fprintf("Percent occultation: %.4f%%\n", percentOccultation * 100)
+fprintf("Daily occultation average: %s\n", string(dailyOccultationAvg))
+fprintf("Occultation duration average: %s\n", string(occultationAvg))
+
+% Plot the durations on time graph (filtered for <5) ---------------------%
+figure
+scatter(combinedOccultation{:,1}, occultationDurations, ".")
+title("Duration of Radio Occultations With Atleast 4 GPS Access")
+xlabel("Start Time of Interval")
+ylabel("Duration")
+grid on
+
+% Assumes May 5th start date
+xticks(datetime(STARTDATE) + hours(0:24:336))
+yticks(minutes(0:0.5:7))
+
+% Plot the starting and stopping latitudes -------------------------------%
+figure
+hold on
+for i = 1:size(combinedOccultation, 1)
+    plot(combinedOccultation{i,3:4}, [occultationDurations(i,1) occultationDurations(i,1)], '-c')
+end
+title("Duration of Radio Occultation Shown as Latitude Intervals")
+xlabel("Latitude (deg)")
+ylabel("Duration")
+grid on
+
+% Turn off the horizontal lines
+ax = gca;
+ax.YGrid = "off";
+
+% Plot the length of latitude intervals ---------------------------------%
+latitudeLength = abs(combinedOccultation{:,4} - combinedOccultation{:,3});
+
+figure
+scatter(combinedOccultation{:,1}, latitudeLength, ".")
+title("Latitude Length of Radio Occultations")
+xlabel("Start Time of Interval")
+xticks(datetime(STARTDATE) + hours(0:24:336))
+
+ylabel("Latitude Length (deg)")
+ylim([-0.3 26.5])
+grid on
---
+++ GetFileFormat.m
@@ -0,0 +1,36 @@
+function [opt] = GetFileFormat(filePath, ELEMENTS)
+%{   
+GETFILEOPTIONS - Returns the options for reading in a file containing 
+access data. This function assumes variable types and names 
+   
+Input: 
+    filePath - Path to the file to import options from
+    ELEMENTS - Array containing all variable names
+   
+Output:
+    opt - Contains the structure of the table, including variable name and
+    type
+%}   
+arguments (Input)
+    filePath
+    ELEMENTS
+end
+
+arguments (Output)
+    opt
+end
+
+    opt = detectImportOptions(filePath);
+    
+    % Set variable types
+    opt.Delimiter = ',';
+    opt.VariableTypes{1} = 'datetime';
+    opt.VariableTypes{2} = 'datetime';
+    opt.VariableTypes{3} = 'double';
+    opt.VariableTypes{4} = 'double';
+    opt.VariableNames = ELEMENTS;
+    
+    % Date time formatting
+    opt = setvaropts(opt, ELEMENTS{1}, 'InputFormat', "dd MMMM yyyy HH:mm:ss.SSS");
+    opt = setvaropts(opt, ELEMENTS{2}, 'InputFormat', "dd MMMM yyyy HH:mm:ss.SSS");
+end
